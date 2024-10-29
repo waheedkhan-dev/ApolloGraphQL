0e565fb28d394aa6b55a91327c4f1acbed044e69
Waheed Khan <waheedkhan6446@gmail.com>
2024-10-29 12:40:00 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ApolloGraphQl
+ 
---
+++ app/src/main/java/com/wk/apollographql/HiltApplication.kt
@@ -0,0 +1,17 @@
+package com.wk.apollographql
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class HiltApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
